f1c701c353b1c0bec21fce9d4ae25c599b012457
Ujwal Basuthkar <97447647+Usbasuthkar@users.noreply.github.com>
2023-06-26 17:57:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def home():
         sunset = "--"
     return render_template("index.html",place = search,description=description,temperature=main_temp,feel_like = feel_like,max=max,min=min,press=press,hum=hum,ws=ws,r=r,sunrise=sunrise,sunset=sunset)
 if __name__ == "__main__":
-    app.run(debug=True,port=8080,host='127.0.0.1')
+    app.run(debug=True,port=5000,host='127.0.0.1')
